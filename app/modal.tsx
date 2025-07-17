508dd4dae77526cc465f94ec32b971f1ad4991f5
Christian Bayan Francia <pipoyfrancia0322@gmail.com>
2025-07-17 11:16:30 +0800

    no googleauth

--- app/modal.tsx
+++ app/modal.tsx
@@ -8,7 +8,7 @@ export default function ModalScreen() {
   return (
     <View className="flex-1 items-center justify-center">
       <Text className="text-xl font-bold">Modal</Text>
-      <View className="my-7 h-px w-4/5 bg-gray-200" />
+      <View className="my-7 h-px w-4/5 bg-gray-100" />
       <EditScreenInfo path="app/modal.tsx" />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
